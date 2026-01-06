f4ba04a2561f5523480719a7e1bd8350b2ab7362
Selçuk Ulusal <115923621+selcukulusal@users.noreply.github.com>
2026-01-06 12:42:08 -0800

    Add files via upload

---
+++ calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Calculator
+
+{
+    public static int number = 1013; public static int number2 = 1013;
+    public static int Add()
+    {return number + number2;}
+
+    public static void Main(string[] args)
+    {int result = Add(); Console.WriteLine("the sum is: " +result);}
+}
